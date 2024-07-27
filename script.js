cfdfeccc221987d688d45777c199f527ff20a9f8
IamAditya-Dubey <sursaritatechknow@gmail.com>
2024-07-27 11:50:36 +0530

    Added swap currency function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let btn = document.querySelector("button");
 let msg = document.querySelector(".msg");
 let fromCurr = document.querySelector(".from select");
 let toCurr = document.querySelector(".to select");
+let exIcon = document.querySelector(".exIcon");
 
 let dropdowns = document.querySelectorAll(".dropdown select");
 
@@ -26,7 +27,6 @@ option.addEventListener("change", (e) => {
 let updateFlag = (element) => {
   let currCode = element.value;
   let countryCode = countryList[currCode];
-  console.log(countryCode);
   let srcUrl = `https://flagsapi.com/${countryCode}/flat/64.png`
   let img = element.parentElement.querySelector("img");
   img.src = srcUrl;
@@ -41,9 +41,7 @@ let exchangeRate = async() => {
   let url = `${BASE}/${fromCurr.value.toLowerCase()}.json`;
   let response = await fetch(url);
   let data = await response.json();
-  console.log(data);
   let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
-  console.log(rate)
   let finalAmt = input.value * rate;
   let lastAmt = finalAmt.toFixed(2);
   msg.innerText = `${input.value} ${fromCurr.value} = ${lastAmt} ${toCurr.value}`;
@@ -56,4 +54,29 @@ btn.addEventListener("click", (e) => {
 
 window.addEventListener("load", () => {
   exchangeRate();
+});
+
+////////Swap FUNCTION///////
+
+let select1 = document.getElementById("select1");
+let select2 = document.getElementById("select2");
+
+let currency1 = fromCurr.value;
+let currency2 = toCurr.value;
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+
+function swapCurr() {
+  let temp = select1.value;
+  select1.value = select2.value;
+  select2.value = temp;
+  currency1 = select1.value;
+  currency2 = select2.value;
+  let imgTemp = img1.src;
+  img1.src = img2.src;
+  img2.src = imgTemp;
+}
+
+exIcon.addEventListener("click", (e) => {
+  swapCurr();
 })
